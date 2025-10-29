f00e97ab4e250290dcec01c8391ecf7b681fa17a
Ola <u56125@student.wat.edu.pl>
2025-10-29 11:01:18 +0100

    wip: added function to added new user

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ def user_info(users_data:list)->None:
     for user in users_data:
         print(f'Twój znajomy {user['name']} z miejscowości {user['location']} opublikował tyle {user['posts']} postów ')
 
+
+def add_user(users_data)->None:
+    name:str=input("Podaj imie nowego znajomego: ")
+    location:str=input("Podaj nazwe miejscowosci: ")
+    posts:int=int(input("Podaj liczbę postów: "))
+    users_data.append({"name":name,"location":location,"posts":posts})
+
 while True:
   tmp_choice:int=int(input("wybierz opcje menu:"))
   if tmp_choice==0:
@@ -16,6 +23,7 @@ while True:
         user_info(users)
   if tmp_choice==2:
        print("wybrano funkcje dodawania znajomych")
+  add_user(users)
   if tmp_choice==3:
      print("wybrano funkcje aktualizowania znajomych")
 
